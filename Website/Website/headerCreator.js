ccf9ad20cea1ae897f23b2eb480da04fd25f7df7
enhorningsqulten <75262324+enhorningsqulten@users.noreply.github.com>
2020-11-30 15:59:11 +0100

    new files

---
+++ Website/Website/headerCreator.js
@@ -0,0 +1,33 @@
+document.write('\
+<div class="head_container">\
+      <div class="head_gradient">\
+      </div>\
+      <div class="head_block">\
+        <a href="index.html">\
+          <div class="logo_button">\
+            <img src="logo.png"></img>\
+          </div>\
+        </a>\
+        <a href="index.html">\
+          <div class="head_button">\
+            <h2>Home</h2>\
+          </div>\
+        </a>\
+        <a href="index.html">\
+          <div class="head_button">\
+            <h2>Gospel</h2>\
+          </div>\
+        </a>\
+        <a href="index.html">\
+          <div class="head_button">\
+            <h2>Rituals</h2>\
+          </div>\
+        </a>\
+        <a href="index.html">\
+          <div class="head_button">\
+            <h2>Contact</h2>\
+          </div>\
+        </a>\
+      </div>\
+    </div>\
+');
