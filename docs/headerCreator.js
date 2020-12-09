907c888eca996cd861179696a37967f024d78f2c
enhorningsqulten <75262324+enhorningsqulten@users.noreply.github.com>
2020-12-09 17:18:59 +0100

    EY lol test

--- docs/headerCreator.js
+++ docs/headerCreator.js
@@ -1,33 +1,62 @@
 document.write('\
-<div class="head_container">\
-      <div class="head_gradient">\
-      </div>\
-      <div class="head_block">\
-        <a href="index.html">\
-          <div class="logo_button">\
-            <img src="logo.png"></img>\
-          </div>\
-        </a>\
-        <a href="index.html">\
-          <div class="head_button">\
-            <h2>Home</h2>\
-          </div>\
-        </a>\
-        <a href="gospel.html">\
-          <div class="head_button">\
-            <h2>Gospel</h2>\
-          </div>\
-        </a>\
-        <a href="index.html">\
-          <div class="head_button">\
-            <h2>Teachings</h2>\
-          </div>\
-        </a>\
-        <a href="index.html">\
-          <div class="head_button">\
-            <h2>Contact</h2>\
-          </div>\
-        </a>\
+<div id="bigscreen">\
+  <div class="head_container">\
+        <div class="head_gradient">\
+        </div>\
+        <div class="head_block">\
+          <a href="index.html">\
+            <div class="logo_button">\
+              <img src="logo.png"></img>\
+            </div>\
+          </a>\
+          <a href="index.html">\
+            <div class="head_button">\
+              <h2>Home</h2>\
+            </div>\
+          </a>\
+          <a href="gospel.html">\
+            <div class="head_button">\
+              <h2>Gospel</h2>\
+            </div>\
+          </a>\
+          <a href="index.html">\
+            <div class="head_button">\
+              <h2>Läran</h2>\
+            </div>\
+          </a>\
+          <a href="kontakt.html">\
+            <div class="head_button">\
+              <h2>Kontakt</h2>\
+            </div>\
+          </a>\
+        </div>\
       </div>\
     </div>\
+    <div id="smallscreen">\
+    <div class="head_container">\
+          <div class="head_gradient">\
+          </div>\
+          <div class="head_block">\
+              <div class="dropdown">\
+              <div class="ddpic">\
+                <button class="dropbtn">\
+                <img src="ddbars.png"></img>\
+                </button>\
+                </div>\
+                <div class="dropdown-content">\
+                <a href="gospel.html">\
+                <div class="head_button">\
+                  <h2>Gospel</h2>\
+                </div>\
+              </a>\
+                </div>\
+              </div>\
+            <a href="index.html">\
+              <div class="logo_button">\
+                <img src="logo.png"></img>\
+              </div>\
+            </a>\
+            </div>\
+          </div>\
+      </div>\
 ');
