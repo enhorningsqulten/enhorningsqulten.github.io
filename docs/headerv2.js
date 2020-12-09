a80b29afe8755dcc219e618a8c114a1da9e3f74e
enhorningsqulten <75262324+enhorningsqulten@users.noreply.github.com>
2020-12-09 18:30:10 +0100

    Test3 for today

---
+++ docs/headerv2.js
@@ -0,0 +1,27 @@
+document.write('\
+<div id="siteheader">\
+        <div class="logo">\
+          <img src="img/logo.png" alt="Logga">\
+        </div>\
+          <div class="links">\
+            <a href="index.html">\
+              <div>Hem</div>\
+          </a>\
+          </div>\
+          <div class="links">\
+            <a href="gospelv2.html">\
+              <div>Gospel</div>\
+          </a>\
+          </div>\
+          <div class="links">\
+            <a href="kontakt.html">\
+              <div>Kontakt</div>\
+          </a>\
+          </div>\
+          <div class="links">\
+            <a href="https://calendar.google.com/calendar/u/2?cid=cXVsdHNjaHRhYmVuQGdtYWlsLmNvbQ">\
+              <div>Kalender</div>\
+          </a>\
+          </div>\
+      </div>\
+');
